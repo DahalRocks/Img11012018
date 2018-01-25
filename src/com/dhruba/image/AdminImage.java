d14de98da910f8a9290f7b5680a1afcc5881f0af
DahalRocks <dhruba.dahal03@gmail.com>
2018-01-25 14:01:54 +0100

    Update AdminImage.java

--- src/com/dhruba/image/AdminImage.java
+++ src/com/dhruba/image/AdminImage.java
@@ -1,5 +1,7 @@
 package com.dhruba.image;
 
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
@@ -8,10 +10,52 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 public class AdminImage {
 	private int imageid;
 	private String imagename;
-	@NotEmpty(message="Image type can not be empty!!")
-	private String imagetype;
+	@NotNull(message="Specify if it has sub image!!")
+	private boolean haveSubimage;
 	private CommonsMultipartFile file;
+	private CommonsMultipartFile randomimagefile;
+	private CommonsMultipartFile similarimagefile;
+	private String randomimage;
+	private String similarimage;
+	private int parentid;
+	private String imagetype;
 	
+	public String getImagetype() {
+		return imagetype;
+	}
+	public void setImagetype(String imagetype) {
+		this.imagetype = imagetype;
+	}
+	public int getParentid() {
+		return parentid;
+	}
+	public void setParentid(int parentid) {
+		this.parentid = parentid;
+	}
+	public String getRandomimage() {
+		return randomimage;
+	}
+	public void setRandomimage(String randomimage) {
+		this.randomimage = randomimage;
+	}
+	public String getSimilarimage() {
+		return similarimage;
+	}
+	public void setSimilarimage(String similarimage) {
+		this.similarimage = similarimage;
+	}
+	public CommonsMultipartFile getRandomimagefile() {
+		return randomimagefile;
+	}
+	public void setRandomimagefile(CommonsMultipartFile randomimagefile) {
+		this.randomimagefile = randomimagefile;
+	}
+	public CommonsMultipartFile getSimilarimagefile() {
+		return similarimagefile;
+	}
+	public void setSimilarimagefile(CommonsMultipartFile similarimagefile) {
+		this.similarimagefile = similarimagefile;
+	}
 	public CommonsMultipartFile getFile() {
 		return file;
 	}
@@ -30,14 +74,12 @@ public class AdminImage {
 	public void setImagename(String imagename) {
 		this.imagename = imagename;
 	}
-	public String getImagetype() {
-		return imagetype;
+	public boolean isHaveSubimage() {
+		return haveSubimage;
 	}
-	public void setImagetype(String imagetype) {
-		this.imagetype = imagetype;
+	public void setHaveSubimage(boolean haveSubimage) {
+		this.haveSubimage = haveSubimage;
 	}
-	
-	
 	
 	
 
