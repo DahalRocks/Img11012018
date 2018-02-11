1d96281ef3b50dc36d7f43c943ec34677d9808c7
DahalRocks <dhruba.dahal03@gmail.com>
2018-02-11 21:53:50 +0100

    Update UserDetail.java

--- src/com/dhruba/user/UserDetail.java
+++ src/com/dhruba/user/UserDetail.java
@@ -1,5 +1,9 @@
 package com.dhruba.user;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +16,13 @@ public class UserDetail {
 	@NotEmpty(message="Education field can not be empty!!")
 	private String education;
 	@NotEmpty(message="Nationality field can not be empty!!")
+	@Size(min = 2, max = 50, message = "Nationality is not correct")
+	@Pattern(regexp="[a-zA-Z]+", message="Nationality can not be numerical or special character")
 	private String nationality;
 	@NotEmpty(message="Email field can not be empty!!")
+	//@Email(message="Email address is not in a proper format!!")
+	@Pattern(regexp="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"  
+   + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message="Email address is not in a proper format!!")
 	private String email;
 	
 	public String getEmail() {
